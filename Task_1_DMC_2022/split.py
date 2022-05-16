bf3a5d9f0d9ffd89820124f79d51f0fc08435289
Jakob Bach <jakob.bach@kit.edu>
2022-05-16 14:03:31 +0200

    Try different splitting strategies for DMC
    All use temporal split, but the question is which user-item combinations
    to consider.

--- Task_1_DMC_2022/split.py
+++ Task_1_DMC_2022/split.py
@@ -19,12 +19,24 @@ if __name__ == '__main__':
     orders = pd.read_csv(DATA_DIR / 'orders.csv', sep='|', parse_dates=['date'])
 
     orders_train = orders[orders['date'] <= LAST_TRAIN_DAY]
-    # Keep all user-item combinations in test set that occur in training set (could also sample):
-    solution = orders_train[['userID', 'itemID']].drop_duplicates()
+    solution = orders_train[['userID', 'itemID']].copy()
+
+    # Option 1: Keep all train user-item combinations in test (as an alternative: sample)
+    # --- no additional code necessary ---
+
+    # # Option 2: Keep only train user-item combinations in test that appear in DMC test set:
+    # solution = solution.merge(pd.read_csv(DATA_DIR / 'submission.csv', sep='|')[['userID', 'itemID']])
+
+    # # Option 3: Keep only user-item combinations in test that appear at least twice in train:
+    # solution = solution.groupby(['userID', 'itemID']).filter(lambda x: len(x) >= 2)
+
+    # Remove repeated orders:
+    solution.drop_duplicates(inplace=True)
     # Find the first date after training period when item is purchased by a user
     first_replenishment = orders[orders['date'] > LAST_TRAIN_DAY].groupby(
         ['userID', 'itemID'])['date'].min().reset_index()
-    # Add this information; note that some items might not be replenished (therefore LEFT join):
+    # Add this information; note that some items might not be replenished, and replenishment of
+    # items not appearing in "solution" is not interesting for us -> therefore LEFT join:
     solution = solution.merge(first_replenishment, how='left')
     # Create target variable by discretizing replenishment date into four weeks + out of test period
     intervals = [LAST_TRAIN_DAY + pd.Timedelta(days=x * 7) for x in range(5)]  # left-open intervals
@@ -34,7 +46,7 @@ if __name__ == '__main__':
     # Order by userID and itemID to remove any temporal information:
     solution.sort_values(by=['userID', 'itemID'], inplace=True)
     # Create submission by setting target column emtpy:
-    submission = solution[['userID', 'itemID']]
+    submission = solution[['userID', 'itemID']].copy()
     submission['prediction'] = float('nan')
 
     orders_train.to_csv(DATA_DIR / 'split_orders.csv', sep='|', index=False)
