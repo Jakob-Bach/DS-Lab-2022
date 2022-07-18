5cc06eb09aea79bb9791268d6a493085a56dfb79
Jakob Bach <jakob.bach@kit.edu>
2022-07-18 13:18:11 +0200

    Add scaling to demo-clustering script
    Data should always be scaled before (distance-based, multi-feature)
    clustering, and scaling also improves clustering quality on our dataset.

--- Task_2_SAT_Solving/cluster_kmeans.py
+++ Task_2_SAT_Solving/cluster_kmeans.py
@@ -8,6 +8,7 @@ import pathlib
 
 import pandas as pd
 import sklearn.cluster
+import sklearn.preprocessing
 
 
 DATA_DIR = pathlib.Path('data/')
@@ -19,11 +20,16 @@ if __name__ == '__main__':
     X_test = pd.read_csv(DATA_DIR / f'{TARGET}_X_test.csv')
     y_train = pd.read_csv(DATA_DIR / f'{TARGET}_y_train.csv')
 
+    test_hashes = X_test['hash']
+    scaler = sklearn.preprocessing.StandardScaler()
+    X_train = scaler.fit_transform(X=X_train.drop(columns='hash'))
+    X_test = scaler.transform(X=X_test.drop(columns='hash'))
+
     model = sklearn.cluster.KMeans(n_clusters=y_train[TARGET].nunique(), random_state=25)
     # fit() + separate predict() (cluster assignment) doesn't work for all sklearn clustering algos;
     # alternatively, one could directly cluster the test data, or cluster train + test together
-    model.fit(X=X_train.drop(columns='hash'))
-    y_test_pred = model.predict(X=X_test.drop(columns='hash'))
-    y_test_pred = pd.DataFrame({'hash': X_test['hash'], TARGET: y_test_pred})
+    model.fit(X=X_train)
+    y_test_pred = model.predict(X=X_test)
+    y_test_pred = pd.DataFrame({'hash': test_hashes, TARGET: y_test_pred})
 
     y_test_pred.to_csv(DATA_DIR / f'{TARGET}_cluster-k-means_prediction.csv', index=False)
